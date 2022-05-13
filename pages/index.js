e021fdf0d3f6303ceb3ac71b4a39014b6e4df674
alkoky <alkoky@gmail.com>
2022-05-13 11:29:37 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Test3</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Test4</a>
         </h1>
 
         <p className={styles.description}>
